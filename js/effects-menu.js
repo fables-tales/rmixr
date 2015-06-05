d1b59ec2d3b76488a2c5d60b94925ebb21e9e40d
Sam Phippen <samphippen@googlemail.com>
2015-06-05 19:27:01 +0200

    Menu

---
+++ js/effects-menu.js
@@ -0,0 +1,57 @@
+(function() {
+    var QueryString = function () {
+        // This function is anonymous, is executed immediately and
+        // the return value is assigned to QueryString!
+        var query_string = {};
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            // If first entry with this name
+            if (typeof query_string[pair[0]] === "undefined") {
+                query_string[pair[0]] = pair[1];
+                // If second entry with this name
+            } else if (typeof query_string[pair[0]] === "string") {
+                var arr = [ query_string[pair[0]], pair[1] ];
+                query_string[pair[0]] = arr;
+                // If third or later entry with this name
+            } else {
+                query_string[pair[0]].push(pair[1]);
+            }
+        }
+        return query_string;
+    }();
+
+    function sendLocalStorageItem(item) {
+        window.localStorage.setItem("message", JSON.stringify(item));
+    }
+
+    function sendAddGainNodeMessage(channel) {
+        var nodeDetails = {
+            effect: {"name": "gain", "params": {"gain": 1.0}},
+            channel: channel
+        };
+        sendLocalStorageItem(nodeDetails);
+    }
+
+    function sendAddTBEQ(channel) {
+        var nodeDetails = {
+            effect: {"name": "threebandEQ", "params": {"midgain": 0.0, "bassgain": 0.0, "treblegain": 0.0}},
+            channel: channel
+        };
+        sendLocalStorageItem(nodeDetails);
+    }
+
+    $(document).ready(function() {
+        var channel = parseInt(QueryString.channel);
+        $("#gain").click(function() {
+            sendAddGainNodeMessage(channel);
+            close();
+        });
+
+        $("#three-band-eq").click(function() {
+            sendAddTBEQ(channel);
+            close();
+        });
+    });
+}());
--- js/ui.js
+++ js/ui.js
@@ -8,7 +8,23 @@ window.DawUI = (function() {
         this.call = function() {
             $(document).ready(function() {
                 initUI();
+
+                setInterval(function() {
+                    var message = window.localStorage.getItem("message");
+                    if (message != "") {
+                        var parsedMessage = JSON.parse(message);
+                        addEffect(parsedMessage);
+                        window.localStorage.setItem("message", "");
+                    }
+                },100);
             });
+        }
+
+        function addEffect(message) {
+            var channel = message.channel;
+            var state = parseHash();
+            state.channels[channel].effects.push(message.effect);
+            window.location.hash = JSON.stringify(state);
         }
 
         function initUI() {
@@ -37,11 +53,20 @@ window.DawUI = (function() {
             return linkElement;
         }
 
-        function showChannelStripSettings(i) {
+        function showChannelStripSettings(channelIndex) {
             var state = parseHash();
-            var channelState = state.channels[i];
+            var channelState = state.channels[channelIndex];
+            var effects = channelState.effects;
             $("#effects-menu-list").empty();
-            $("#effects-menu-list").append("<li class='pure-menu-item'><span class='pure-menu-link'>New effect</span></li>");
+            for (var i = 0; i < effects.length; i++) {
+                var effect = effects[i];
+                $("#effects-menu-list").append("<li class='pure-menu-item'><span class='pure-menu-link'>" + effect.name + "</span></li>");
+            }
+            var newEffectNode = $("<li class='pure-menu-item'><span class='pure-menu-link'>New effect</span></li>");
+            $(newEffectNode).click(function() {
+                var myWindow = window.open("/menu.html?channel=" + channelIndex, "MsgWindow", "width=200, height=300");
+            });
+            $("#effects-menu-list").append(newEffectNode);
         };
 
         function inflateTemplate(template, params) {
