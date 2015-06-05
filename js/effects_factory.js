2f326d409254f0cd4b7c0ed5bc9cf68d95419006
Ben Fields <me@benfields.net>
2015-06-05 19:11:37 +0200

    wired in the eq

--- js/effects_factory.js
+++ js/effects_factory.js
@@ -9,6 +9,10 @@ window.EffectsFactory = (function() {
             gain.connect(sink);
         };
 
+        knownEffects.threebandEQ = function(source, sink, params) {
+            threebandEQ(source, sink, params.bassgain, params.midgain, params.treblegain);
+        };
+
         this.call = function(source, sink, effectState) {
             var effectFactory = knownEffects[effectState.name];
             effectFactory(source, sink, effectState.params);
--- js/fx/threeband_eq.js
+++ js/fx/threeband_eq.js
@@ -10,14 +10,14 @@ window.threebandEQ = function(source, sink, bassgain, midgain, treblegain) {
     treble.connect(sink);
 
     bass.type = "lowshelf";
-    bass.frequency.value = 400;
+    bass.frequency.value = 100;
     bass.gain.value = bassgain;
     mid.type = "peaking";
-    mid.frequency.value = 2000;
-    mid.q.value = 1;
-    mid.gain.value = midvalue;
+    mid.frequency.value = 1000;
+    mid.Q.value = 1;
+    mid.gain.value = midgain;
     treble.type = "highshelf";
     treble.frequency.value = 4000;
-    treble.gain.value = treblevalue;
+    treble.gain.value = treblegain;
 
 };
