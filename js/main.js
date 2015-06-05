f8091a2fa2a9e11c406d05ce933e5863868f1b1b
Sam Phippen <samphippen@googlemail.com>
2015-06-05 15:10:44 +0100

    URL change state mods

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 (function() {
+
+    var oldHash = "";
+
+    function loadStateFromHash(hash) {
+        var state = JSON.parse(hash.substring(1));
+        stripCollection.updateState(state);
+    }
+
+    function respondToHashChange() {
+        var hash = window.location.hash;
+        if (hash != oldHash) {
+            loadStateFromHash(hash);
+            oldHash = hash;
+        }
+
+    };
+
     var samples = [
         "source_audio/derezzed_bass.mp3",
         "source_audio/derezzed_layer_1.mp3",
@@ -7,20 +24,12 @@
 
 
     window.audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-    var stripCollection = ChannelStripCollection.factory(samples);
+    window.stripCollection = ChannelStripCollection.factory(samples);
     stripCollection.startAll(new BrowserAudioSink(audioContext.destination));
 
     var tgain = 0;
 
     setInterval(function() {
-        console.log("XB");
-        tgain = 1-tgain;
-        var effects = [
-            {"name": "gain", "params": {"gain": tgain == 0 ? 0.2 : tgain}}
-        ];
-        stripCollection.updateState({
-            channels: [{effects: effects}, {effects: effects}, {effects: effects}]
-        })
-    }, 1500);
+        respondToHashChange();
+    },100);
 })();
