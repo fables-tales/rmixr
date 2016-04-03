160db1f3c1136145b1f2bc482fff309c45210b1a
Sam Phippen <samphippen@googlemail.com>
2016-04-03 17:46:26 -0400

    mix tabs and spaces because I'm great
    /cc @hughrawlinson I am an excellent and 100% good dev.

--- js/fx/converb.js
+++ js/fx/converb.js
@@ -1,11 +1,11 @@
 window.converb = function(source, sink, dry, wet) {
     var convolver = new tuna.Convolver({
-                        highCut: 22050,                         //20 to 22050
-                        lowCut: 20,                             //20 to 22050
-                        dryLevel: dry,                            //0 to 1+
-                        wetLevel: wet,                            //0 to 1+
-                        level: 1,                               //0 to 1+, adjusts total output of both wet and dry
-                        impulse: "js/tuna/impulses/impulse_rev.wav",    //the path to your impulse response
+                        highCut: 22050,                                         //20 to 22050
+                        lowCut: 20,                                             //20 to 22050
+                        dryLevel: dry,                                          //0 to 1+
+                        wetLevel: wet,                                          //0 to 1+
+                        level: 1,                                               //0 to 1+, adjusts total output of both wet and dry
+                        impulse: "js/tuna/impulses/impulse_rev.wav",            //the path to your impulse response
                         bypass: 0
                     });
     source.connect(convolver.input);
